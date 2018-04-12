24e8f6a21e4e8349019ab857546cc17775a42973
xindekai <34989038+xindekai@users.noreply.github.com>
2018-04-12 12:04:10 +0100

    Update appX.js
    adding script functionality

--- appX.js
+++ appX.js
@@ -1,1 +1,12 @@
+const aboutButton = document.getElementById('show-about');
+const aboutContainer = document.getElementById('about-panel');
+aboutButton.addEventListener('mouseenter',showAB);
+aboutButton.addEventListener('mouseout',hideAB);
+function showAB() {
+  aboutContainer.style.visibility = "visible";
+}
+function hideAB() {
+  aboutContainer.style.visibility = "hidden";
+}
+
 
